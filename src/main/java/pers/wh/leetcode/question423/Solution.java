e2814f2000b2dd7400259410bf0b8e089e5c04cf
wanghan.wang <wanghan.wang@qunar.com>
2017-04-26 10:45:56 +0800

    solution 43,423

--- src/main/java/pers/wh/leetcode/question423/Solution.java
+++ src/main/java/pers/wh/leetcode/question423/Solution.java
@@ -2,6 +2,14 @@ package pers.wh.leetcode.question423;
 
 /**
  * Created by wanghan on 2017/4/26.
+ *
+ * Given a non-empty string containing an out-of-order English representation of digits 0-9, output the digits in ascending order.
+ *
+ * Note:
+ * 1.Input contains only lowercase English letters.
+ * 2.Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as "abc" or "zerone" are not permitted.
+ * 3.Input length is less than 50,000.
+ *
  */
 public class Solution {
 }
